c40b67e1978b3b7c55510c8284748efba6b6b203
Negm <Karimyasser8@gmail.com>
2024-01-20 00:52:40 +0200

    Minor naming adjustments, attributed author of the macros xacro

--- src/coorbot_description/launch/display.launch.py
+++ src/coorbot_description/launch/display.launch.py
@@ -1,8 +1,8 @@
 import xacro
+import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
-import os
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import SetEnvironmentVariable
@@ -18,7 +18,7 @@ def generate_launch_description():
                     executable='rviz2',
                     output='screen',
                     parameters=[{'use_sim_time': True }],
-                    # arguments=['-d', os.path.join(get_package_share_directory('coorbot_description'), 'urdf', 'Robot.urdf')]
+                    arguments=['-d', os.path.join(get_package_share_directory('coorbot_description'), 'config', 'coorbot_gazebo.rviz')]
                 )
     
     
